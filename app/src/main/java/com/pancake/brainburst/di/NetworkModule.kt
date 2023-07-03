2e34946868822bab60742f988ba384586396fe38
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-04 02:28:43 +0300

    fix dependencies issues

--- app/src/main/java/com/pancake/brainburst/di/NetworkModule.kt
+++ app/src/main/java/com/pancake/brainburst/di/NetworkModule.kt
@@ -8,7 +8,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.annotation.Signed
 import javax.inject.Singleton
 
 

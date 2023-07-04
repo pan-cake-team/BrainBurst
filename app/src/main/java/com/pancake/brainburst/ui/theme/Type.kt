1cddfb81272328153a4db4c544eafe2d22deb22e
ameeramjed <pr.ameer98@gmail.com>
2023-07-04 13:48:51 +0300

    add typography design system

--- app/src/main/java/com/pancake/brainburst/ui/theme/Type.kt
+++ app/src/main/java/com/pancake/brainburst/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.pancake.brainburst.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pancake.brainburst.R
 
-// Set of Material typography styles to start with
+private val Poppins = FontFamily(
+    Font(R.font.poppins_light, FontWeight.W300),
+    Font(R.font.poppins_regular, FontWeight.W400),
+    Font(R.font.poppins_medium, FontWeight.W500),
+    Font(R.font.poppins_bold, FontWeight.W600),
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = fontSizesSecondary,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = fontSizePrimary,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = fontSizesCaption,
         letterSpacing = 0.5.sp
+    ),
+
+
     )
-    */
-)

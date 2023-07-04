57701b94f5ca7b9e3916993f6c4328ab4e15dd1f
kareem <skrym826@gmail.com>
2023-07-04 11:11:10 +0300

    add BrandColor and fonts

--- app/src/main/java/com/pancake/brainburst/ui/theme/Color.kt
+++ app/src/main/java/com/pancake/brainburst/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BrandB500 = Color(0xFF77AAFE)
+val BrandB300 = Color(0xFFD5E5FF)
+val BrandB100 = Color(0xFFEBF2FF)
--- app/src/main/java/com/pancake/brainburst/ui/theme/Type.kt
+++ app/src/main/java/com/pancake/brainburst/ui/theme/Type.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.pancake.brainburst.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,38 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+
+val poppins = FontFamily(
+    Font(R.font.poppins, FontWeight.Normal),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
+
+val GooglePoppinsTypography = Typography(
+    headlineSmall = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    )
+
 )

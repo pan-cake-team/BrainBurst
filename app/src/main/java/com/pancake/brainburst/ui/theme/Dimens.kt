a2be92098e06c79cbdadfaf92a98d33736da088f
ameeramjed <pr.ameer98@gmail.com>
2023-07-04 13:46:39 +0300

    add dimens design system

---
+++ app/src/main/java/com/pancake/brainburst/ui/theme/Dimens.kt
@@ -0,0 +1,25 @@
+package com.pancake.brainburst.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+val zero: Dp = 0.dp
+
+val spacing: Dp = 4.dp
+val spacingSmall: Dp = 8.dp
+val spacingXSmall: Dp = 12.dp
+val spacingMedium: Dp = 16.dp
+val spacingLarge: Dp = 24.dp
+val spacingExtraLarge: Dp = 32.dp
+val spacingHuge: Dp = 48.dp
+
+val fontSizePrimary: TextUnit = 14.sp
+val fontSizesSecondary: TextUnit = 16.sp
+val fontSizesThird: TextUnit = 22.sp
+val fontSizesCaption: TextUnit = 12.sp
+val fontSizesSmall: TextUnit = 10.sp
+val fontSizesTitle: TextUnit = 24.sp
+val fontSizesTitleExtra: TextUnit = 32.sp

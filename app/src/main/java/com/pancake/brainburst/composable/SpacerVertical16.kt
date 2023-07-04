0a284149885dd265d3a3ace81ee272aafcd6c125
kareem <skrym826@gmail.com>
2023-07-04 16:49:54 +0300

    add spacers

---
+++ app/src/main/java/com/pancake/brainburst/composable/SpacerVertical16.kt
@@ -0,0 +1,12 @@
+package com.pancake.brainburst.composable
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerVertical16() {
+    Spacer(modifier = Modifier.height(16.dp))
+}

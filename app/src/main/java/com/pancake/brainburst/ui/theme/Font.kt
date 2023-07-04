72eda26e09ab1d0f20baa371803047e5b4d90b7c
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-07-04 22:25:47 +0300

    add design system components

---
+++ app/src/main/java/com/pancake/brainburst/ui/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.pancake.brainburst.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.pancake.brainburst.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(R.font.poppins_semi_bold, weight = FontWeight.SemiBold)
+)
+
+val NerkoOne = FontFamily(
+    Font(R.font.nerko_one_regular, weight = FontWeight.Normal),
+)

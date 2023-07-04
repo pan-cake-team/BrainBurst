d2867eb4cd9dd41005c9ec7f5bb1473cd8d2c084
ali3030 <ali3030@users.noreply.github.com>
2023-07-05 01:30:40 +0300

    adding dependencies and adding android:name=".BrainBurst" in mainfest

--- app/src/main/java/com/pancake/brainburst/MainActivity.kt
+++ app/src/main/java/com/pancake/brainburst/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pancake.brainburst
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,39 +9,51 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.pancake.brainburst.screens.welecome.WelcomeScreen
 import com.pancake.brainburst.ui.theme.BrainBurstTheme
+import com.pancake.brainburst.ui.theme.TWhite
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BrainBurstTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            WelcomeScreen()
+
+            WindowCompat.setDecorFitsSystemWindows(window, true)
+
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent, darkIcons = true
+                )
+
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BrainBurstTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        BrainBurstTheme {
+            Greeting("Android")
+        }
     }
 }

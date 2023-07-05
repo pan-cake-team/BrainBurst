2b1d51d432f4ae87de9da2f9571fa031d0ccb65d
ali3030 <ali3030@users.noreply.github.com>
2023-07-06 02:06:40 +0300

    fixing and editing WelcomeScreen

--- app/src/main/java/com/pancake/brainburst/MainActivity.kt
+++ app/src/main/java/com/pancake/brainburst/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.pancake.brainburst
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.pancake.brainburst.screens.welecome.WelcomeScreen
 import com.pancake.brainburst.ui.theme.BrainBurstTheme
-
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,17 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WelcomeScreen()
-
-            WindowCompat.setDecorFitsSystemWindows(window, true)
-
-            val systemUiController = rememberSystemUiController()
-            SideEffect {
-                systemUiController.setStatusBarColor(
-                    color = Color.Transparent, darkIcons = true
-                )
-
-
-            }
         }
     }
 
--- app/src/main/java/com/pancake/brainburst/screens/welecome/WelcomeScreen.kt
+++ app/src/main/java/com/pancake/brainburst/screens/welecome/WelcomeScreen.kt
@@ -1,100 +1,53 @@
 package com.pancake.brainburst.screens.welecome
-import androidx.annotation.DrawableRes
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pancake.brainburst.R
 import com.pancake.brainburst.ui.theme.BrainBurstTheme
-import com.pancake.brainburst.ui.theme.Cyan500
 import com.pancake.brainburst.ui.theme.NerkoOne
+import com.pancake.brainburst.ui.theme.textSize14
+import com.pancake.brainburst.ui.theme.textSize32
 
 @Preview
 @Composable
 fun WelcomeScreen(viewModel: WelcomeScreenViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CustomSpacer(height = 72.dp)
+        Spacer(modifier = Modifier.weight(0.3f))
         CustomImage(resourceId = R.drawable.crown_star, contentDesc = "crown")
-        CustomText(text = state.heightsScore.toString(), size = 32,fontFamily = NerkoOne)
-        CustomSpacer(height = 14.dp)
-        CustomText(text = "Your heights score", size = 14)
-        CustomSpacer(height = 56.dp)
+        CustomText(text = state.heightsScore.toString(), textSize32, fontFamily = NerkoOne)
+        Spacer(modifier = Modifier.weight(0.1f))
+        CustomText(text = "Your heights score", textSize14)
+        Spacer(modifier = Modifier.weight(0.2f))
         Box {
-            CustomImage(resourceId = R.drawable.winner, contentDesc = "winner")
-            CustomImage(resourceId = R.drawable.image_2, contentDesc = "winner2")
+            Image(
+                painter = painterResource(id = R.drawable._3297415_5157958_1,),
+                contentDescription = null,
+                modifier = Modifier
+            )
         }
-        CustomSpacer(height = 100.dp)
+        Spacer(modifier = Modifier.weight(0.5f))
         PlayButton()
     }
 }
-
-@Composable
-fun CustomSpacer(height: Dp) {
-    Spacer(modifier = Modifier.height(height))
-}
-
-
-@Composable
-fun CustomImage(@DrawableRes resourceId: Int, contentDesc: String) {
-    Image(
-        painter = painterResource(id = resourceId),
-        contentDescription = contentDesc,
-        modifier = Modifier.graphicsLayer { this.alpha = alpha }
-    )
-}
-
-@Composable
-fun CustomText(text: String, size: Int, fontFamily: FontFamily = FontFamily.Default) {
-    Text(
-        text = text,
-        fontSize = size.sp,
-        fontFamily = fontFamily
-    )
-}
-
-@Composable
-fun PlayButton() {
-    Button(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)
-            .height(56.dp),
-        onClick = { },
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Cyan500,
-        ),
-    ) {
-        Text(
-            text = "Let's PLay",
-            fontSize = 14.sp,
-        )
-    }
-}
-
 
 @Preview(showBackground = true)
 @Composable

5ab06030873aa71f2594f10e03a5214514d51586
kareem <skrym826@gmail.com>
2023-07-06 19:52:51 +0300

    relocate gameScreen

--- app/src/main/java/com/pancake/brainburst/screens/gameScreen/gameScreen.kt
+++ app/src/main/java/com/pancake/brainburst/screens/gameScreen/gameScreen.kt
@@ -1,4 +1,4 @@
-package com.pancake.brainburst.screens
+package com.pancake.brainburst.screens.gameScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -20,14 +21,27 @@ import com.pancake.brainburst.composable.AnswerCard
 import com.pancake.brainburst.composable.GameQuestionCard
 import com.pancake.brainburst.composable.SpacerVertical16
 import com.pancake.brainburst.composable.SpacerVertical24
-import com.pancake.brainburst.screens.constants.question
+import com.pancake.brainburst.screens.gameScreen.constants.question
 import com.pancake.brainburst.ui.theme.LightBackground
 
 @Composable
 fun GameScreen() {
+
     val clickedState = remember {
         mutableStateOf(false)
     }
+    val isTimerOut = remember {
+        mutableStateOf(false)
+    }
+    GameContent(clickedState, isTimerOut)
+
+}
+
+@Composable
+private fun GameContent(
+    clickedState: MutableState<Boolean>,
+    isTimerOut: MutableState<Boolean>,
+) {
 
     Column(
         modifier = Modifier
@@ -43,7 +57,11 @@ fun GameScreen() {
             SpacerVertical16()
         }
         SpacerVertical24()
-        GameQuestionCard(question = "Lorem ipsum dolor sit amet consectetur Placerat ut sit urna .")
+        GameQuestionCard(
+            question = "Lorem ipsum dolor sit amet consectetur Placerat ut sit urna .",
+            isTimerOut,
+            isItemCLicked = clickedState
+        )
         SpacerVertical24()
 
         LazyVerticalGrid(
@@ -52,16 +70,21 @@ fun GameScreen() {
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             item {
-                AnswerCard(letter = "A", answer = "Jupiter", isCLicked = clickedState)
+                AnswerCard(
+                    letter = "A",
+                    answer = "Jupiter",
+                    isCLicked = clickedState,
+                    isTimerOut
+                )
             }
             item {
-                AnswerCard("B", answer = "Jupiter", isCLicked = clickedState)
+                AnswerCard("B", answer = "Jupiter", isCLicked = clickedState, isTimerOut)
             }
             item {
-                AnswerCard("C", answer = "Jupiter", isCLicked = clickedState)
+                AnswerCard("C", answer = "Jupiter", isCLicked = clickedState, isTimerOut)
             }
             item {
-                AnswerCard("D", answer = "Jupiter", isCLicked = clickedState)
+                AnswerCard("D", answer = "Jupiter", isCLicked = clickedState, isTimerOut)
             }
 
 

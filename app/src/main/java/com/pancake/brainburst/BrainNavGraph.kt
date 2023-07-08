286c7810a594d62a1dfae905bee20232d8642208
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-07-08 18:44:06 +0300

    win/lose screen done

--- app/src/main/java/com/pancake/brainburst/BrainNavGraph.kt
+++ app/src/main/java/com/pancake/brainburst/BrainNavGraph.kt
@@ -4,17 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.pancake.brainburst.ui.screens.GameScreen2
 import com.pancake.brainburst.ui.screens.gameScreen.GameScreen
-import com.pancake.brainburst.ui.screens.gameScreen.GameScreen2
 import com.pancake.brainburst.ui.screens.home.HomeScreen
 import com.pancake.brainburst.ui.screens.savedQuestions.SavedQuestionScreen
 import com.pancake.brainburst.ui.screens.welecome.WelcomeScreen
 import com.pancake.brainburst.ui.screens.winScreen.LoseScreen
-import com.pancake.brainburst.ui.screens.winScreen.WinScreen
+import com.pancake.brainburst.ui.screens.winScreen.winRout
 
 @Composable
 fun BrainNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = AppDestination.HomeScreen.screen) {
+    NavHost(navController = navController, startDestination = "gameScreenTwo") {
 
         composable(AppDestination.WelcomeScreen.screen) { WelcomeScreen() }
 
@@ -22,11 +22,11 @@ fun BrainNavGraph(navController: NavHostController) {
 
         composable(AppDestination.GameScreenOne.screen) { GameScreen() }
 
-        composable(AppDestination.GameScreenTwo.screen) { GameScreen2() }
+        composable(AppDestination.GameScreenTwo.screen) { GameScreen2(navController) }
 
         composable(AppDestination.SavedQuestion.screen) { SavedQuestionScreen() }
 
-        composable(AppDestination.WinScreen.screen) { WinScreen() }
+        winRout(navController)
 
         composable(AppDestination.LoseScreen.screen) { LoseScreen() }
     }

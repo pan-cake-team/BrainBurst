e22a2d6c2ba1878b716028340eda343ced2f7ae1
HassanAyman <hassan3ayman@gmail.com>
2023-07-08 14:02:29 +0200

    refactor welcome screen

--- app/src/main/java/com/pancake/brainburst/ui/screens/welecome/WelcomeScreen.kt
+++ app/src/main/java/com/pancake/brainburst/ui/screens/welecome/WelcomeScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -14,12 +17,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pancake.brainburst.R
 import com.pancake.brainburst.ui.theme.BrainBurstTheme
+import com.pancake.brainburst.ui.theme.Cyan300
 import com.pancake.brainburst.ui.theme.NerkoOne
+import com.pancake.brainburst.ui.theme.space24
+import com.pancake.brainburst.ui.theme.space56
 import com.pancake.brainburst.ui.theme.textSize14
 import com.pancake.brainburst.ui.theme.textSize32
 
@@ -27,6 +33,14 @@ import com.pancake.brainburst.ui.theme.textSize32
 @Composable
 fun WelcomeScreen(viewModel: WelcomeScreenViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
+
+    welcomeScreenContent(
+        state
+    )
+}
+
+@Composable
+fun welcomeScreenContent(state: WelcomeScreenUIState) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -34,32 +48,54 @@ fun WelcomeScreen(viewModel: WelcomeScreenViewModel = hiltViewModel()) {
     ) {
         Spacer(modifier = Modifier.weight(0.3f))
 
-        Image(
-            painter = painterResource(id = R.drawable.crown_star),
-            contentDescription = "crown",
-            modifier = Modifier
-        )
-        Text(
-            text = state.heightsScore.toString(),
-            fontSize = textSize32,
-            fontFamily = NerkoOne
-        )
-        Spacer(modifier = Modifier.weight(0.1f))
-        Text(
-            text = "Your heights score",
-            fontSize = textSize14,
-
+        if (state.hasScore()){
+            Image(
+                painter = painterResource(id = R.drawable.crown_star),
+                contentDescription = "crown",
+                modifier = Modifier
             )
+            Text(
+                text = state.heightsScore.toString(),
+                fontSize = textSize32,
+                fontFamily = NerkoOne
+            )
+            Spacer(modifier = Modifier.weight(0.1f))
+            Text(
+                text = stringResource(R.string.your_heights_score),
+                fontSize = textSize14,
+            )
+        }
         Spacer(modifier = Modifier.weight(0.2f))
         Box {
             Image(
-                painter = painterResource(id = R.drawable._3297415_5157958_1),
+                painter = painterResource(id = R.drawable.welcome_vector),
                 contentDescription = null,
                 modifier = Modifier
             )
         }
         Spacer(modifier = Modifier.weight(0.5f))
         PlayButton()
+    }
+}
+
+@Composable
+fun PlayButton() {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(space24)
+            .height(space56),
+
+
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Cyan300,
+        ),
+    ) {
+        Text(
+            text = stringResource(R.string.let_s_play),
+            fontSize = textSize14,
+        )
     }
 }
 
